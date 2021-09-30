cbded0099372bed5faffd4d66a8ad02c170299ce
Brahayan Gil <stivensgt@hotmail.com>
2021-09-30 14:35:08 -0500

    Agregar informacion a la tabla

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const vehiculosBackend =[
     {
@@ -32,6 +34,7 @@ const Vehiculos = () => {
     const [mostrartabla,setMostrartabla] = useState(true)
     const [textoBoton,setTextoBoton] = useState('Crear nuevo vehiculo')
     const [vehiculos,setVehiculos] = useState([])
+    const [colorBoton,setColorBoton] = useState('indigo')
     
     useEffect(()=>{
         //obetener lista de vehiculos desde el backend
@@ -44,8 +47,10 @@ const Vehiculos = () => {
     useEffect(()=>{
         if(mostrartabla){
             setTextoBoton('Crear nuevo Vehiculo')
+            setColorBoton('indigo')
         } else {
             setTextoBoton('Mostrar todos los vehiculos')
+            setColorBoton('red')
         }
     },[mostrartabla])
     return (
@@ -56,14 +61,20 @@ const Vehiculos = () => {
                 setMostrartabla(!mostrartabla)
             }} 
             
-            className='bg-indigo-500 p-5 text-white rounded-full m-6 w-28 self-end'>
+            className={`bg-${colorBoton}-500 p-5 text-white rounded-full m-6 w-28 self-end`}>
             {textoBoton}</button>
             </div>
             {mostrartabla ? <TablaVehiculos listaVehiculos={vehiculos}/> :
-            <FormularioVehiculos/>
+            <FormularioVehiculos 
+            listaVehiculos={vehiculos}
+            mostrartabla={setMostrartabla} 
+            agregarVehiculo={setVehiculos}/>
             }
+            <ToastContainer
+            position="bottom-center"
+            autoClose={5000}/>
 
-
+             
 
         </div> 
     )
@@ -98,17 +109,71 @@ const TablaVehiculos = ({listaVehiculos})=>{
     )
 }
 
-const FormularioVehiculos = ()=>{
+const FormularioVehiculos = ({mostrartabla, listaVehiculos,agregarVehiculo})=>{
+
+    const [nombre,setNombre] = useState()
+    const [marca,setMarca] = useState()
+    const [modelo,setModelo] = useState()
+
+    const enviarBackend = () =>{
+        console.log('nombre',nombre,'marca',marca,'modelo',modelo)
+        toast.success('Vehiculo creado con exito')
+        mostrartabla(true)
+        agregarVehiculo([...listaVehiculos,
+            {nombre:nombre,marca:marca,modelo:modelo}])
+
+    }
     
     return(
         <div className='flex flex-col justify-center items-center'>
             <h2 className='text-gray-800 text-2xl font-extrabold'>Crear nuevo vehiculo</h2>
             <form className='grid grid-cols-2'>
-                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' type="text" />
-                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' type="text" />
-                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' type="text" />
-                <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' type="text" />
-                <button className='col-span-2 bg-green-400 p-2 rounded-lg shadow-md hover:bg-green-600 text-white'> Guardar vehiculo</button>
+                <label htmlFor="nombre">
+                    Nombre del vehiculo
+                <input 
+                    name='nombre'
+                    className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' 
+                    type="text"
+                    placeholder='Corolla'
+                    value= {nombre}
+                    onChange= {(e)=>(setNombre(e.target.value))}
+                />
+                </label>
+                <label htmlFor="marca">
+                    Marca del vehiculo
+                    <select 
+                        value= {marca}
+                        onChange= {(e)=>(setMarca(e.target.value))}
+                        className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' name='marca' >
+                        <option disabled>Seleccion una opcion</option>
+                        <option >Renault</option>
+                        <option >Toyota</option>
+                        <option >Ford</option>
+                        <option >Mazda</option>
+                        <option >Chevrolet</option>
+                    </select>
+                </label>
+                <label htmlFor="modelo">
+                    Modelo del vehiculo
+                <input 
+                    name='modelo'
+                    className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2 ' 
+                    type="number"
+                    min= {1992}
+                    max= {2022}
+                    placeholder='2014'
+                    value= {modelo}
+                    onChange= {(e)=>(setModelo(e.target.value))}
+                />
+                
+                </label>
+               
+                <button 
+                type='button'
+                onClick={()=>{
+                    enviarBackend()
+                }}
+                className='col-span-2 bg-green-400 p-2 rounded-lg shadow-md hover:bg-green-600 text-white'> Guardar vehiculo</button>
             </form>
         </div>
     )
