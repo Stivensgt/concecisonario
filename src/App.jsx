9d4b3c03ab746fd28689edec7b852f168be33b89
Brahayan Gil <stivensgt@hotmail.com>
2021-10-03 20:24:50 -0500

    Use context

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { DarkModeContext } from 'context/darkMode';
 import AuthLayout from 'layout/AuthLayout';
 import PrivateLayout from 'layout/PrivateLayout';
 import Publiclayout from 'layout/PublicLayout';
@@ -9,10 +10,18 @@ import Login from 'pages/Login';
 import Registro from 'pages/Registro';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'styles/App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(()=>{
+    console.log('Modo dark:' ,darkMode)
+  },[darkMode])
   return (
 
+
+      <DarkModeContext.Provider value={{darkMode,setDarkMode}} > 
       <Router>
         <Switch>
           <Route path = {['/admin','/admin/vehiculos','/admin/clientes']}>
@@ -55,8 +64,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
-     
-    
+      </DarkModeContext.Provider>
   );
 }
 
