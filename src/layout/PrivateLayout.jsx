b5239542f0b5e0e179ebeaa7e0e89d9336054977
Brahayan Gil <stivensgt@hotmail.com>
2021-10-03 21:41:20 -0500

    Sidebar resposive

--- src/layout/PrivateLayout.jsx
+++ src/layout/PrivateLayout.jsx
@@ -1,12 +1,14 @@
 
 import Sidebar from 'components/Sidebar'
+import SidebarResponsive from 'components/SidebarResponsive'
 import React from 'react'
 
 const PrivateLayout = ({children}) => {
     return (
         <div className='flex w-screen h-screen'>
-            <div className= 'flex flex-nowrap h-full w-full'>
+            <div className= 'flex flex-col md:flex-row flex-nowrap h-full w-full'>
             <Sidebar/>
+            <SidebarResponsive/>
             <main className= 'flex w-full overflow-y-scroll items-center justify-center'>{children}</main>
             </div>
         </div>
